6209bc26bfb941e3dce5e26c77196d9e4a09e6dd
Mohamed <alxmohamedemadeldin@gmail.com>
2024-01-08 12:26:17 -0800

    Solution updated

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+"""subclass checker"""
 
 
 def inherits_from(obj, a_class):
-    '''the object is an instance of a class that inherited (directly or indirectly)
-    obj: an object
-    a_class: a class
-    returns None
-    '''
-    return type(obj) != a_class and isinstance(obj, a_class)
+    """returns subclass yes"""
+    if type(obj) == a_class:
+        return False
+    return issubclass(type(obj), a_class)
