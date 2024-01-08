a297f9ba76b3abdc582b01c3087ded81c10f5ad5
Mohamed <alxmohamedemadeldin@gmail.com>
2024-01-08 12:21:31 -0800

    Solution added

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+
+def inherits_from(obj, a_class):
+    '''the object is an instance of a class that inherited (directly or indirectly)
+    obj: an object
+    a_class: a class
+    returns None
+    '''
+    return type(obj) != a_class and isinstance(obj, a_class)
