d33543d77562e3f3e4da22eec5dd17bf7b05758a
Mohamed <alxmohamedemadeldin@gmail.com>
2024-03-13 19:56:23 -0700

    Solved

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+if (process.argv[2] && Number(process.argv[2])) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    const size = process.argv[2];
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const firstNumber = Number(process.argv[2]);
+const secondNumber = Number(process.argv[3]);
+
+add(firstNumber, secondNumber);
+
+function add (a, b) {
+  console.log(a + b);
+}
