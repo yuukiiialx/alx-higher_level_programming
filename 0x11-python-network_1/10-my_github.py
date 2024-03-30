d338adcf0f12925962c63de75cf197c70d36dbfd
Mohamed <alxmohamedemadeldin@gmail.com>
2024-03-29 20:05:11 -0700

    Updated

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
-"""
-Module to access to the GitHub API and uses the information
-"""
-import requests
-from requests.auth import HTTPBasicAuth
-from sys import argv
-
-
-def main(argv):
-    """
-    Script that takes your GitHub credentials (username and password) and
-    uses the GitHub API to display your id.
-    """
-    user = argv[1]
-    password = argv[2]
-    response = requests.get('https://api.github.com/user',
-                            auth=HTTPBasicAuth(user, password))
-    try:
-        profile_info = response.json()
-        print(profile_info['id'])
-    except:
-        print('None')
+"""display the id variable found in the header of the response."""
 
 if __name__ == "__main__":
-    main(argv)
+    import requests
+    import sys
+    headers = {'Authorization': f'Bearer {sys.argv[2]}',
+               'X-GitHub-Api-Version': '2022-11-28',
+               'Accept': 'application/vnd.github.v3+json'}
+    url = 'https://api.github.com/user'
+    req = requests.get(url, headers=headers)
+    print(req.json().get('id'))
